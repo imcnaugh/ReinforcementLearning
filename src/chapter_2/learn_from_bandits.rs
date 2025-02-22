670cf4138c7b6f7ce382837fbfae0f93e0693a92
Ian McNaughton <IanMcNaugh@gmail.com>
2025-02-22 17:12:58 -0600

    a different way of calculating an avarage, a bit more math, but needs 1 less input (the sum of the rewards) the sum would have a possibility of overflowing, so i suppose the second method is better

--- src/chapter_2/learn_from_bandits.rs
+++ src/chapter_2/learn_from_bandits.rs
@@ -22,7 +22,9 @@ fn learn(bandits: &KArmedBandit, explore_rate: f32, steps: u32) -> f32 {
         count_of_times_actions_taken[action_to_take_id] += 1;
         total_rewards_of_actions_taken[action_to_take_id] += reward;
 
-        estimated_action_values[action_to_take_id] = total_rewards_of_actions_taken[action_to_take_id] / count_of_times_actions_taken[action_to_take_id] as f32;
+        estimated_action_values[action_to_take_id] = calc_new_expected_action_value_old(
+            count_of_times_actions_taken[action_to_take_id],
+            total_rewards_of_actions_taken[action_to_take_id]);
 
         total_reward += reward;
     });
@@ -34,6 +36,14 @@ fn learn(bandits: &KArmedBandit, explore_rate: f32, steps: u32) -> f32 {
     }
     
     total_reward
+}
+
+fn calc_new_expected_action_value_old(count_of_times_actions_taken: u32, total_rewards_of_actions_taken: f32) -> f32 {
+    total_rewards_of_actions_taken / count_of_times_actions_taken as f32
+}
+
+fn calc_new_expected_action_value_new(current_estimate: f32, count_of_times_actions_taken: u32, reward: f32) -> f32 {
+    current_estimate + (1.0 / count_of_times_actions_taken as f32) * (reward - current_estimate)
 }
 
 fn get_explore_action_id(num_of_bandits: usize) -> usize {
@@ -57,6 +67,7 @@ fn get_exploit_action_id(estimated_action_values: &Vec<f32>) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::time::Instant;
 
     fn setup_bandit() -> KArmedBandit {
         KArmedBandit::rand_new(10)
@@ -75,5 +86,40 @@ mod tests {
 
         let total_reward = learn(&bandit, 0.0, 10000);
         println!("explore rate: {}, total reward: {}", 0.0, total_reward);
+    }
+
+    #[test]
+    fn test_averaging_functions() {
+        let mut rng = rand::thread_rng();
+        let random_floats: Vec<f32> = (0..10000000).map(|_| rng.random()).collect();
+        let sum: f32 = random_floats.iter().sum();
+
+        let existing_average = calc_new_expected_action_value_old(random_floats.len() as u32, random_floats.iter().sum::<f32>());
+
+        let new_reward: f32 = rng.random();
+
+        let start = Instant::now();
+
+        println!("existing average: {}, new reward: {}", existing_average, new_reward);
+
+        let old_estimate = calc_new_expected_action_value_old(
+            random_floats.len() as u32 + 1,
+            sum + new_reward
+        );
+
+        let duration = start.elapsed(); // Measure the elapsed time
+        println!("Time elapsed old method: {:?}", duration);
+
+        let new_estimate = calc_new_expected_action_value_new(
+            existing_average,
+            random_floats.len() as u32 + 1,
+            new_reward
+        );
+
+        let duration = start.elapsed(); // Measure the elapsed time
+        println!("Time elapsed new method: {:?}", duration);
+
+        println!("old estimate: {}, new estimate: {}", old_estimate, new_estimate);
+        assert_eq!(new_estimate, old_estimate);
     }
 }
