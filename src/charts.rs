ff8d5738b17a4a3e1b04e2518717c57307ccf795
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-02-24 19:32:26 -0600

    it will be good to have a chart lib handy

---
+++ src/charts.rs
@@ -0,0 +1,49 @@
+use plotters::prelude::*;
+
+fn wat() -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("output/my_chart.png", (640, 480)).into_drawing_area();
+
+    std::fs::create_dir_all("output")?;
+
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("y=x^2", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x)),
+            &RED,
+        ))?
+        .label("y = x")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn wat_that_thang_do() {
+        match wat() {
+            Ok(_) => {assert!(true, "wat")}
+            Err(e) => {panic!("wat {}", e)}
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod chapter_2;
+mod charts;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
