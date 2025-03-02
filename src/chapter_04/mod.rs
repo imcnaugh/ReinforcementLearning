f4d8a711d7a6812d80c88a2049e33f7ff831f812
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-03-02 14:42:13 -0600

    setting up some test data for calculations

--- src/chapter_04/mod.rs
+++ src/chapter_04/mod.rs
@@ -4,22 +4,37 @@ mod state;
 pub use action::Actions;
 pub use state::State;
 
-fn iterative_policy_evaluation(states: Vec<f32>, theta: f32) -> f32 {
-    loop {
-        let mut delta: f32 = 0_f32;
-        states.iter().for_each(|state| {
-            let v = state;
-        });
-        if delta < theta {
-            break;
-        }
-    }
+fn iterative_policy_evaluation() -> f32 {
     0.0
 }
 
 #[cfg(test)]
 mod tests {
-
+    use super::*;
     #[test]
-    fn test_iterative_policy_evaluation() {}
+    fn test_iterative_policy_evaluation() {
+        let mut initial_state = State::new();
+
+        let state_a1_1 = State::new();
+        let state_a1_2 = State::new();
+        let state_a1_3 = State::new();
+
+        let state_a2_1 = State::new();
+        let state_a2_2 = State::new();
+        let state_a2_3 = State::new();
+
+        let mut action_1 = Actions::new();
+        let mut action_2 = Actions::new();
+
+        action_1.add_possible_next_state(0.5, &state_a1_1);
+        action_1.add_possible_next_state(0.25, &state_a1_2);
+        action_1.add_possible_next_state(0.25, &state_a1_3);
+
+        action_2.add_possible_next_state(0.33, &state_a2_1);
+        action_2.add_possible_next_state(0.33, &state_a2_2);
+        action_2.add_possible_next_state(0.33, &state_a2_3);
+
+        initial_state.add_action(&action_1);
+        initial_state.add_action(&action_2);
+    }
 }
