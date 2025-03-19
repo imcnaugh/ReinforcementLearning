651e74cf7659698a4bc2ceb103a7285aaf0bcea6
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-03-19 15:30:52 -0500

    the weighted importance sampling function

--- src/chapter_05/importance_sampling.rs
+++ src/chapter_05/importance_sampling.rs
@@ -35,6 +35,37 @@ pub fn ordinary_importance_sampling<TP: Policy, BP: Policy>(
         })
 }
 
+/// # Weighted importance sampling
+///
+/// from page 105 of the book
+///
+/// ## Args
+/// runs: a vector of tuples, the first element of the tuple is the state actions pairs taken, and the second element is the reward. //TODO find a better way to represent all of this
+/// target_policy
+/// behavior_policy
+pub fn weighted_importance_sampling<TP: Policy, BP: Policy>(
+    runs: &Vec<(&Vec<(&str, &str)>, f64)>,
+    target_policy: &TP,
+    behavior_policy: &BP,
+) -> Result<f64, String> {
+    let mut numerator = 0.0;
+    let mut denominator = 0.0;
+
+    runs.iter().try_for_each(|run| {
+        let importance_sampling_ratio =
+            calculate_importance_sampling_ratio(run.0, target_policy, behavior_policy)?;
+        numerator += run.1 * importance_sampling_ratio;
+        denominator += importance_sampling_ratio;
+        Ok::<(), String>(())
+    })?;
+
+    if denominator == 0.0 {
+        return Ok(0.0);
+    }
+
+    Ok(numerator / denominator)
+}
+
 pub fn calculate_importance_sampling_ratio<TP: Policy, BP: Policy>(
     state_action_paris: &Vec<(&str, &str)>,
     target_policy: &TP,
@@ -126,5 +157,33 @@ mod tests {
             ordinary_importance_sampling(&runs, &target_policy, &behavior_policy);
 
         assert_eq!(ordinary_importance_sampling.unwrap(), 20.0);
+    }
+
+    #[test]
+    fn test_weighted_importance_sampling() {
+        let state_action_pairs = vec![("state_1", "state_1 -> state_2")];
+
+        let mut target_policy = DeterministicPolicy::new();
+        target_policy.set_action_for_state(&state_action_pairs[0].0, state_action_pairs[0].1);
+
+        let mut behavior_policy = StochasticPolicy::new();
+        behavior_policy
+            .set_state_actions_probabilities_using_e_soft_probabilities(
+                state_action_pairs[0].0,
+                vec![
+                    String::from(state_action_pairs[0].1),
+                    String::from("some_other_action"),
+                ],
+                1.0, // 1 is impractical, but it makes the math clean for testing.
+                state_action_pairs[0].1.to_string(),
+            )
+            .unwrap();
+
+        let runs = vec![(&state_action_pairs, 10.0)];
+
+        let weighted_importance_sampling =
+            weighted_importance_sampling(&runs, &target_policy, &behavior_policy);
+
+        assert_eq!(weighted_importance_sampling.unwrap(), 10.0);
     }
 }
