e11af79797d11735f43e2c89d00e6c412616d810
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-03-19 15:19:57 -0500

    dam i love it when things build on top of each other like this

--- src/chapter_05/importance_sampling.rs
+++ src/chapter_05/importance_sampling.rs
@@ -1,4 +1,39 @@
 use crate::chapter_05::policy::Policy;
+use crate::service::calc_average;
+
+/// # Ordinary importance sampling
+///
+/// from page 104 of the book
+///
+/// ## Args
+/// runs: a vector of tuples, the first element of the tuple is the state actions pairs taken, and the second element is the reward. //TODO find a better way to represent all of this
+/// target_policy
+/// behavior_policy
+pub fn ordinary_importance_sampling<TP: Policy, BP: Policy>(
+    runs: &Vec<(&Vec<(&str, &str)>, f64)>,
+    target_policy: &TP,
+    behavior_policy: &BP,
+) -> Result<f64, String> {
+    runs.iter()
+        .enumerate()
+        .try_fold(0.0, |current_average, (index, run)| {
+            let iteration = index + 1;
+            let state_action_pairs = run.0;
+            let reward = run.1;
+            let importance_ratio = calculate_importance_sampling_ratio(
+                state_action_pairs,
+                target_policy,
+                behavior_policy,
+            )?;
+            let adjusted_reward = reward * importance_ratio;
+
+            Ok(calc_average(
+                current_average,
+                iteration as i32,
+                adjusted_reward,
+            ))
+        })
+}
 
 pub fn calculate_importance_sampling_ratio<TP: Policy, BP: Policy>(
     state_action_paris: &Vec<(&str, &str)>,
@@ -63,5 +98,33 @@ mod tests {
         );
 
         assert_eq!(importance_ratio.unwrap(), 2.0);
+    }
+
+    #[test]
+    fn test_ordinary_importance_sampling() {
+        let state_action_pairs = vec![("state_1", "state_1 -> state_2")];
+
+        let mut target_policy = DeterministicPolicy::new();
+        target_policy.set_action_for_state(&state_action_pairs[0].0, state_action_pairs[0].1);
+
+        let mut behavior_policy = StochasticPolicy::new();
+        behavior_policy
+            .set_state_actions_probabilities_using_e_soft_probabilities(
+                state_action_pairs[0].0,
+                vec![
+                    String::from(state_action_pairs[0].1),
+                    String::from("some_other_action"),
+                ],
+                1.0, // 1 is impractical, but it makes the math clean for testing.
+                state_action_pairs[0].1.to_string(),
+            )
+            .unwrap();
+
+        let runs = vec![(&state_action_pairs, 10.0)];
+
+        let ordinary_importance_sampling =
+            ordinary_importance_sampling(&runs, &target_policy, &behavior_policy);
+
+        assert_eq!(ordinary_importance_sampling.unwrap(), 20.0);
     }
 }
