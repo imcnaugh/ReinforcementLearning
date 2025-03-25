cce0ebff6bae520aa99bec1465e0bf38c3518439
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-03-25 17:17:35 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use eframe::egui;
-use egui::TextBuffer;
+use egui::{TextBuffer, Ui};
+use simple_chess::piece::ChessPiece;
+use simple_chess::ChessGame;
 use std::path::Path;
+use simple_chess::chess_game_state_analyzer::GameState;
 use ReinforcementLearning::chapter_05::policy::{DeterministicPolicy, Policy};
 use ReinforcementLearning::chapter_05::race_track::learning::MonteCarloOffPolicyLearner;
 use ReinforcementLearning::chapter_05::race_track::racer::Racer;
@@ -13,16 +16,11 @@ fn main() {
         ..Default::default()
     };
 
-    let path = Path::new("resources/tracks/simple_racetrack.txt");
-    let track = parse_track_from_file(path).unwrap();
-
     eframe::run_native(
         "My egui App",
         options,
         Box::new(|_cc| {
             let mut app = MyApp::new();
-            app.set_track(track);
-            app.do_learning();
             Ok(Box::new(app))
         }),
     )
@@ -30,119 +28,97 @@ fn main() {
 }
 
 struct MyApp {
-    track: Option<RaceTrack>,
-    sim_starting_x: String,
-    sim_starting_y: String,
-    target_policy: Option<DeterministicPolicy>,
+    chess_game: ChessGame,
+    selected_square: Option<(usize, usize)>,
 }
 
 impl MyApp {
     pub fn new() -> Self {
         Self {
-            track: None,
-            sim_starting_x: String::from("0"),
-            sim_starting_y: String::from("0"),
-            target_policy: None,
+            chess_game: ChessGame::new(),
+            selected_square: None,
         }
-    }
-
-    pub fn set_track(&mut self, track: RaceTrack) {
-        self.track = Some(track);
     }
 
     pub fn do_learning(&mut self) {
-        if self.track.is_none() {
-            panic!("No track set");
+        // let mut learner = MonteCarloOffPolicyLearner::new(starting_states, 1.0);
+        //
+        // learner.learn_for_episodes(1000000);
+    }
+
+    fn square_selected(&mut self, row: usize, col: usize) {
+        match self.selected_square {
+            None => {
+                self.selected_square = Some((row, col));
+                match self.chess_game.get_game_state() {
+                    GameState::InProgress { legal_moves, turn } => {
+
+                    }
+                    GameState::Check { legal_moves, turn } => {}
+                    GameState::Checkmate { winner } => {}
+                    GameState::Stalemate => {}
+                }
+            },
+            Some(_) => {}
         }
-        let track = self.track.as_ref().unwrap();
+        println!("Square selected: ({}, {})", row, col);
+    }
 
-        let starting_states: Vec<Racer> = track
-            .get_start_positions()
-            .iter()
-            .map(|starting_position| {
-                let starting_position = (starting_position.0 as i32, starting_position.1 as i32);
-                Racer::new(starting_position, track)
-            })
-            .collect();
-
-        let mut learner = MonteCarloOffPolicyLearner::new(starting_states, 1.0);
-
-        learner.learn_for_episodes(1000000);
-
-        self.target_policy = Some(learner.get_target_policy().clone());
+    fn draw_chess_board(&mut self, ui: &mut Ui) {
+        ui.vertical_centered(|ui| {
+            ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+            (0..self.chess_game.get_board().get_height()).rev().for_each(|y| {
+                ui.horizontal(|ui| {
+                    ui.add_sized(
+                        [40.0, 40.0],
+                        egui::Label::new(egui::RichText::new((y + 1).to_string()).size(30.0)),
+                    );
+                    ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+                    (0..self.chess_game.get_board().get_width()).for_each(|x| {
+                        let piece = self.chess_game.get_board().get_piece_at_space(x, y);
+                        let square_color = if (x + y) % 2 == 0 {
+                            egui::Color32::from_rgb(181, 181, 181)
+                        } else {
+                            egui::Color32::from_rgb(138, 144, 145)
+                        };
+                        let piece_as_char = match piece {
+                            None => " ",
+                            Some(p) => p.as_utf_str(),
+                        };
+                        if ui.add_sized(
+                            [40.0, 40.0],
+                            egui::Button::new(
+                                egui::RichText::new(piece_as_char)
+                                    .monospace()
+                                    .color(egui::Color32::from_rgb(0, 0, 0))
+                                    .size(30.0),
+                            )
+                                .frame(false)
+                                .corner_radius(0.0)
+                                .fill(square_color),
+                        ).clicked() {
+                            self.square_selected(x, y);
+                        };
+                    });
+                });
+            });
+            ui.horizontal(|ui| {
+                ui.add_sized([40.0, 40.0], egui::Label::new(""));
+                ('A'..='H').for_each(|x| {
+                    ui.add_sized(
+                        [40.0, 40.0],
+                        egui::Label::new(egui::RichText::new(x).size(30.0)),
+                    );
+                });
+            });
+        });
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let mut track_string = match &self.track {
-            None => String::new(),
-            Some(t) => t.to_string(),
-        };
-
-        if ctx.input(|i| i.key_pressed(egui::Key::ArrowUp)) {
-            if let Ok(value) = self.sim_starting_y.parse::<i32>() {
-                self.sim_starting_y = (value - 1).to_string();
-            }
-        }
-        if ctx.input(|i| i.key_pressed(egui::Key::ArrowDown)) {
-            if let Ok(value) = self.sim_starting_y.parse::<i32>() {
-                self.sim_starting_y = (value + 1).to_string();
-            }
-        }
-        if ctx.input(|i| i.key_pressed(egui::Key::ArrowLeft)) {
-            if let Ok(value) = self.sim_starting_x.parse::<i32>() {
-                self.sim_starting_x = (value - 1).to_string();
-            }
-        }
-        if ctx.input(|i| i.key_pressed(egui::Key::ArrowRight)) {
-            if let Ok(value) = self.sim_starting_x.parse::<i32>() {
-                self.sim_starting_x = (value + 1).to_string();
-            }
-        }
-
-        let player_position = (
-            self.sim_starting_x.parse::<i32>().unwrap_or(0),
-            self.sim_starting_y.parse::<i32>().unwrap_or(0),
-        );
-
-        let action_by_state = self
-            .target_policy
-            .as_ref()
-            .unwrap()
-            .pick_action_for_state(&format!("{}_{}_0_0", player_position.0, player_position.1))
-            .unwrap_or("no");
-
-        track_string = track_string
-            .lines()
-            .enumerate()
-            .map(|(y, line)| {
-                line.chars()
-                    .enumerate()
-                    .map(|(x, ch)| {
-                        if (x as i32, y as i32) == player_position {
-                            '^'
-                        } else {
-                            ch
-                        }
-                    })
-                    .collect::<String>()
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(egui::RichText::new("Hello world!").monospace());
-            ui.label(egui::RichText::new(&track_string).monospace());
-            ui.horizontal(|ui| {
-                ui.label("Starting X:");
-                ui.text_edit_singleline(&mut self.sim_starting_x)
-            });
-            ui.horizontal(|ui| {
-                ui.label("Starting Y:");
-                ui.text_edit_singleline(&mut self.sim_starting_y);
-            });
-            ui.label(egui::RichText::new(&format!("Action: {}", action_by_state)).monospace());
+            self.draw_chess_board(ui);
         });
     }
 }
