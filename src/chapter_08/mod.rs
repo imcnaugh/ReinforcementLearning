505572d03ec874d1863503f773e77fd34efbeaf7
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-09 21:46:59 -0500

    cargo fmt

--- src/chapter_08/mod.rs
+++ src/chapter_08/mod.rs
@@ -1,9 +1,15 @@
-use std::collections::HashMap;
-use rand::prelude::IteratorRandom;
 use crate::attempts_at_framework::v1::policy::{EGreedyPolicy, Policy};
 use crate::attempts_at_framework::v1::state::State;
+use rand::prelude::IteratorRandom;
+use std::collections::HashMap;
 
-pub fn tabular_dyna_q<S: State>(iteration_count: usize, states: Vec<S>, discount_rate: f64, size_step_parameter: f64, n: usize) {
+pub fn tabular_dyna_q<S: State>(
+    iteration_count: usize,
+    states: Vec<S>,
+    discount_rate: f64,
+    size_step_parameter: f64,
+    n: usize,
+) {
     let state_map: HashMap<String, S> = states.iter().map(|s| (s.get_id(), s.clone())).collect();
     let mut state_action_values: HashMap<String, f64> = HashMap::new();
     let mut model: HashMap<(String, String), (f64, String)> = HashMap::new();
@@ -14,34 +20,42 @@ pub fn tabular_dyna_q<S: State>(iteration_count: usize, states: Vec<S>, discount
         let state = states.iter().choose(&mut rng).unwrap().clone();
         let action = match policy.select_action_for_state(&state.get_id()) {
             Ok(a) => a,
-            Err(_) => state.get_actions().iter().choose(&mut rng).unwrap().clone()
+            Err(_) => state.get_actions().iter().choose(&mut rng).unwrap().clone(),
         };
 
         let state_action_id = get_state_action_id(&state.get_id(), &action);
         let (reward, next_state) = state.take_action(&action);
         let current_state_action_value = state_action_values.get(&state_action_id).unwrap_or(&0.0);
-        let next_state_best_action_value = get_max_value_of_state_actions(&state_action_values, &next_state).unwrap_or(0.0);
-        let new_state_action_value = current_state_action_value + (
-            size_step_parameter * (reward + (discount_rate * next_state_best_action_value) - current_state_action_value)
-            );
+        let next_state_best_action_value =
+            get_max_value_of_state_actions(&state_action_values, &next_state).unwrap_or(0.0);
+        let new_state_action_value = current_state_action_value
+            + (size_step_parameter
+                * (reward + (discount_rate * next_state_best_action_value)
+                    - current_state_action_value));
         state_action_values.insert(state_action_id.clone(), new_state_action_value);
-        model.insert((state.get_id(), action), (new_state_action_value, next_state.get_id()));
+        model.insert(
+            (state.get_id(), action),
+            (new_state_action_value, next_state.get_id()),
+        );
 
         (0..n).for_each(|_| {
-            let ((s,a), (r, ns)) = model.iter().choose(&mut rng).unwrap();
+            let ((s, a), (r, ns)) = model.iter().choose(&mut rng).unwrap();
             let s_a_id = get_state_action_id(s, a);
             let current_s_a_value = state_action_values.get(&s_a_id).unwrap_or(&0.0);
             let ns = state_map.get(ns).unwrap().clone();
-            let best_ns_value = get_max_value_of_state_actions(&state_action_values, &ns).unwrap_or(0.0);
-            let new_s_a_value = current_s_a_value + (
-                size_step_parameter * (r + (discount_rate * best_ns_value) - current_s_a_value)
-                );
+            let best_ns_value =
+                get_max_value_of_state_actions(&state_action_values, &ns).unwrap_or(0.0);
+            let new_s_a_value = current_s_a_value
+                + (size_step_parameter * (r + (discount_rate * best_ns_value) - current_s_a_value));
             state_action_values.insert(s_a_id.clone(), new_s_a_value);
         });
     });
 }
 
-fn get_max_value_of_state_actions<S: State>(state_action_values: &HashMap<String, f64>, state: &S) -> Option<f64> {
+fn get_max_value_of_state_actions<S: State>(
+    state_action_values: &HashMap<String, f64>,
+    state: &S,
+) -> Option<f64> {
     let actions = state.get_actions();
     let mut max_value: Option<f64> = None;
     for action in actions {
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ mod chapter_04;
 pub mod chapter_05;
 pub mod chapter_06;
 pub mod chapter_07;
+mod chapter_08;
 pub mod chess_state;
 pub mod service;
-mod chapter_08;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
