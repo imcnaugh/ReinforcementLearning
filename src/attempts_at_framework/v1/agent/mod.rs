7a3328274ef27a4d975e34e0ee32839264f1cffe
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-11 15:31:52 -0500

    starting to try an impliment an n step with planning

--- src/attempts_at_framework/v1/agent/mod.rs
+++ src/attempts_at_framework/v1/agent/mod.rs
@@ -1,7 +1,9 @@
 mod n_step_sarsa;
+mod n_step_sarsa_with_planning;
 mod q_learning;
 mod sarsa_0;
 
 pub use n_step_sarsa::NStepSarsa;
+pub use n_step_sarsa_with_planning::NStepSarsaWithPlanningBuilder;
 pub use q_learning::QLearning;
 pub use sarsa_0::SarsaZero;
