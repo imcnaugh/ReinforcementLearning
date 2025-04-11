031b8f2845a90dd7fb09c94a76234d25a398973b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-11 16:21:46 -0500

    no n step with planning is a duplication of effort, i should be doing singl step sarsa or q learning, bah, i should really be reading more

--- src/attempts_at_framework/v1/agent/mod.rs
+++ src/attempts_at_framework/v1/agent/mod.rs
@@ -1,9 +1,7 @@
 mod n_step_sarsa;
-mod n_step_sarsa_with_planning;
 mod q_learning;
 mod sarsa_0;
 
 pub use n_step_sarsa::NStepSarsa;
-pub use n_step_sarsa_with_planning::NStepSarsaWithPlanningBuilder;
 pub use q_learning::QLearning;
 pub use sarsa_0::SarsaZero;
