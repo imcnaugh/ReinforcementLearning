4155634c216de4b3b1802a64090bf374144240d5
Ian McNaughton <IanMcNaugh@gmail.com>
2025-04-14 12:57:40 -0500

    there it is, it beat martin, ill be damed

--- src/attempts_at_framework/v1/agent/heuristic_search.rs
+++ src/attempts_at_framework/v1/agent/heuristic_search.rs
@@ -7,46 +7,20 @@ use simple_chess::piece::{ChessPiece, PieceType};
 
 pub fn get_best_action(game: &mut ChessGame, depth: usize) -> String {
     let player_color = game.get_current_players_turn();
-    let moves = match game.get_game_state() {
-        GameState::InProgress { legal_moves, .. } => legal_moves,
-        GameState::Check { legal_moves, .. } => legal_moves,
-        GameState::Checkmate { .. } => Vec::new(),
-        GameState::Stalemate => Vec::new(),
-    };
 
-    let idk = moves.iter().map(|&m| {
-        let move_as_string = simple_chess::codec::long_algebraic_notation::encode_move_as_long_algebraic_notation(&m);
-        game.make_move(m);
-        let value: f64 = match game.get_game_state() {
-            GameState::InProgress { .. } => {
-                idk(game, depth, player_color)
-            }
-            GameState::Check { .. } => {
-                idk(game, depth, player_color)
-            }
-            GameState::Checkmate { .. } => 1000.0,
-            GameState::Stalemate => 0.0,
-        };
+    let idk = idk(game, depth, player_color);
+    println!("moves and values: {:?}", idk);
 
-        game.undo_last_move();
+    let max_value = idk.iter().map(|x| x.0).fold(f64::MIN, f64::max);
+    let best_moves: Vec<_> = idk.iter().filter(|x| x.0 == max_value).cloned().collect();
 
-        (move_as_string, value)
-    }).collect::<Vec<(String, f64)>>();
-
-
-    let max_value = idk.iter().map(|x| x.1).fold(f64::MIN, f64::max);
-    let best_moves: Vec<_> = idk.iter().filter(|x| x.1 == max_value).cloned().collect();
-    println!("Best moves: {:?}", best_moves);
-
-    best_moves.iter().choose(&mut rand::rng()).unwrap().0.clone()
+    best_moves.iter().choose(&mut rand::rng()).unwrap().1.clone()
 }
 
-fn idk(game: &mut ChessGame, depth: usize, player_color: Color) -> f64 {
+fn idk(game: &mut ChessGame, depth: usize, player_color: Color) -> Vec<(f64, String)> {
     if depth == 0 {
-        return 0.0;
+        return vec![(0.0, String::new())];
     }
-
-    let players_turn = game.get_current_players_turn() == player_color;
 
     let moves = match game.get_game_state() {
         GameState::InProgress { legal_moves, .. } => legal_moves,
@@ -56,20 +30,37 @@ fn idk(game: &mut ChessGame, depth: usize, player_color: Color) -> f64 {
     };
 
     moves.iter().map(|m| {
+        let move_as_string = simple_chess::codec::long_algebraic_notation::encode_move_as_long_algebraic_notation(&m);
         let mut take_reward= match m {
             ChessMoveType::Move { taken_piece, .. } => get_taken_piece_reward(taken_piece),
             ChessMoveType::EnPassant { .. } => 1.0,
             ChessMoveType::Castle { .. } => 0.0,
         };
 
+        if game.get_current_players_turn() != player_color {
+            take_reward *= -1.0;
+        }
+
         game.make_move(*m);
 
-        let mut continuing_reward = match game.get_game_state() {
+        let continuing_reward = match game.get_game_state() {
             GameState::InProgress { .. } => {
-                idk(game, depth - 1, player_color)
+                let v = idk(game, depth - 1, player_color);
+                let x = if player_color == game.get_current_players_turn() {
+                    f64::max
+                } else {
+                    f64::min
+                };
+                v.iter().map(|a|a.0).reduce(x).unwrap()
             }
             GameState::Check { .. } => {
-                idk(game, depth - 1, player_color)
+                let mut v = idk(game, depth - 1, player_color);
+                let x = if player_color == game.get_current_players_turn() {
+                    f64::max
+                } else {
+                    f64::min
+                };
+                v.iter().map(|a|a.0).reduce(x).unwrap()
             }
             GameState::Checkmate { .. } => {
                 1000.0
@@ -78,8 +69,10 @@ fn idk(game: &mut ChessGame, depth: usize, player_color: Color) -> f64 {
         };
 
         game.undo_last_move();
-        take_reward + continuing_reward
-    }).sum::<f64>() / moves.len() as f64
+
+        let total_reward = take_reward + continuing_reward;
+        (total_reward, move_as_string)
+    }).collect()
 }
 
 fn get_taken_piece_reward(taken_piece: &Option<ChessPiece>) -> f64 {
