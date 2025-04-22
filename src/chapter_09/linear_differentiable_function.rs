3b5882754d9612b2992602877a5a6ad2e60669e2
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-22 11:51:49 -0500

    ohh that refactor diles in nice

--- src/chapter_09/linear_differentiable_function.rs
+++ src/chapter_09/linear_differentiable_function.rs
@@ -1,5 +1,6 @@
 use crate::attempts_at_framework::v1::policy::Policy;
 use crate::attempts_at_framework::v2::state::State;
+use egui::Key::N;
 use rand::prelude::IteratorRandom;
 use std::collections::VecDeque;
 
@@ -38,15 +39,7 @@ pub fn monte_carlo_stochastic_gradient_decent<S: State, P: Policy>(
         let mut states_and_rewards: Vec<(S, f64)> = Vec::new();
 
         while !current_state.is_terminal() {
-            let action = match policy.select_action_for_state(&current_state.get_id()) {
-                Ok(a) => a,
-                Err(_) => current_state
-                    .get_actions()
-                    .iter()
-                    .choose(&mut rand::rng())
-                    .unwrap()
-                    .clone(),
-            };
+            let action = select_action(&current_state, &policy);
             let (reward, next_state) = current_state.take_action(&action);
             states_and_rewards.push((next_state.clone(), reward));
             current_state = next_state;
@@ -75,15 +68,7 @@ pub fn semi_gradient_td0<S: State, P: Policy>(
     (0..episode_count).for_each(|_| {
         let mut current_state = starting_state.clone();
         while !current_state.is_terminal() {
-            let action = match policy.select_action_for_state(&current_state.get_id()) {
-                Ok(a) => a,
-                Err(_) => current_state
-                    .get_actions()
-                    .iter()
-                    .choose(&mut rand::rng())
-                    .unwrap()
-                    .clone(),
-            };
+            let action = select_action(&starting_state, &policy);
 
             let (reward, next_state) = current_state.take_action(&action);
 
@@ -125,15 +110,7 @@ pub fn n_step_semi_gradient_td<S: State, P: Policy>(
 
         for current_timestep in 0..i32::MAX {
             if current_timestep < termination_time.unwrap_or(i32::MAX) {
-                let action = match policy.select_action_for_state(&starting_state.get_id()) {
-                    Ok(a) => a,
-                    Err(_) => starting_state
-                        .get_actions()
-                        .iter()
-                        .choose(&mut rand::rng())
-                        .unwrap()
-                        .clone(),
-                };
+                let action = select_action(&starting_state, &policy);
                 let (reward, next_state) = current_state.take_action(&action);
                 rewards.push(reward);
                 states.push(next_state.clone());
@@ -172,6 +149,68 @@ pub fn n_step_semi_gradient_td<S: State, P: Policy>(
             if time_step_to_update == (termination_time.unwrap_or(i32::MAX) - 1) {
                 break;
             }
+        }
+    });
+
+    weights
+}
+
+fn select_action<S: State, P: Policy>(starting_state: &S, policy: &P) -> String {
+    match policy.select_action_for_state(&starting_state.get_id()) {
+        Ok(a) => a,
+        Err(_) => starting_state
+            .get_actions()
+            .iter()
+            .choose(&mut rand::rng())
+            .unwrap()
+            .clone(),
+    }
+}
+
+pub fn n_step_semi_gradient_td_my_refactor<S: State, P: Policy>(
+    starting_state: S,
+    policy: P,
+    discount_rate: f64,
+    learning_rate: f64,
+    episode_count: usize,
+    n: usize,
+) -> Vec<f64> {
+    let mut weights = vec![0.0; starting_state.get_values().len()];
+
+    (0..episode_count).for_each(|_| {
+        let mut current_state = starting_state.clone();
+        let mut next_state: Option<S> = None;
+        let mut queue: VecDeque<Option<(f64, Vec<f64>)>> = VecDeque::new();
+        (0..n).for_each(|_| queue.push_front(None));
+        let mut sliding_reward_total = 0.0;
+
+        while !queue.is_empty() {
+            if !current_state.is_terminal() {
+                let action = select_action(&current_state, &policy);
+                let (reward, ns) = current_state.take_action(&action);
+                if !ns.is_terminal() {
+                    queue.push_front(Some((reward, current_state.get_values().clone())));
+                }
+                next_state = Some(ns);
+                sliding_reward_total += reward;
+            }
+            let back_of_queue = queue.pop_back().unwrap();
+            if let Some((reward, values)) = back_of_queue {
+                let expected = sliding_reward_total
+                    + (discount_rate.powi(n as i32)
+                        * linear_differentiable_function(&current_state.get_values(), &weights));
+                let new_weights = weight_update(&values, &weights, learning_rate, expected);
+                weights = new_weights;
+
+                let reward_to_remove_from_sliding_window = discount_rate.powi(n as i32) * reward;
+                sliding_reward_total -= reward_to_remove_from_sliding_window;
+            }
+
+            if let Some(ns) = next_state {
+                current_state = ns;
+                next_state = None;
+            }
+            sliding_reward_total *= discount_rate;
         }
     });
 
@@ -407,6 +446,63 @@ mod tests {
             "output/chapter9/thousand_state_random_walk-n_step.png",
         ));
         line_chart_builder.set_title(format!("Thousand state random walk {} step", n));
+        line_chart_builder.set_x_label("State".to_string());
+        line_chart_builder.set_y_label("Value".to_string());
+        line_chart_builder.add_data(LineChartData::new(
+            "Expected".to_string(),
+            vec![(0.0, -1.0), (number_of_states as f32, 1.0)],
+            ShapeStyle::from(&RED),
+        ));
+        line_chart_builder.add_data(LineChartData::new(
+            "State values".to_string(),
+            data_points,
+            ShapeStyle::from(&BLUE),
+        ));
+
+        line_chart_builder.create_chart().unwrap();
+    }
+
+    #[test]
+    fn random_walk_semi_gradient_n_step_from_refactor() {
+        let number_of_states = 1000;
+        let discount_rate = 1.0;
+        let learning_rate = 0.001;
+        let episode_count = 10000;
+        let n = 10;
+
+        // let value_function = generate_simple_value_function(number_of_states);
+        let value_function = generate_state_aggregation_value_function(number_of_states, 50);
+
+        let state_factory = WalkStateFactory::new(number_of_states, 100, value_function).unwrap();
+
+        let starting_state = state_factory.get_starting_state();
+        let learned_weights = n_step_semi_gradient_td_my_refactor(
+            starting_state,
+            RandomPolicy::new(),
+            discount_rate,
+            learning_rate,
+            episode_count,
+            n,
+        );
+
+        let data_points = (0..number_of_states)
+            .map(|i| {
+                let state = state_factory.generate_state_and_reward_for_id(i as i32).1;
+                let value = linear_differentiable_function(&state.get_values(), &learned_weights);
+                (i as f32, value as f32)
+            })
+            .collect();
+
+        println!("learned weights: {:?}", learned_weights);
+
+        let mut line_chart_builder = LineChartBuilder::new();
+        line_chart_builder.set_path(PathBuf::from(
+            "output/chapter9/thousand_state_random_walk-n_step_refactored.png",
+        ));
+        line_chart_builder.set_title(format!(
+            "Thousand state random walk {} step - Refactored",
+            n
+        ));
         line_chart_builder.set_x_label("State".to_string());
         line_chart_builder.set_y_label("Value".to_string());
         line_chart_builder.add_data(LineChartData::new(
