6bc17dd00085bc4cd889ac7df5c1aeddda0a22e3
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-29 10:24:53 -0500

    eh lets punt the input layer problem, I have been chating with the tool and it has reccomeded some code to better calculate out loss functions, lets give that a shot

--- src/attempts_at_framework/v2/artificial_neural_network/model/mod.rs
+++ src/attempts_at_framework/v2/artificial_neural_network/model/mod.rs
@@ -15,6 +15,43 @@ where
     N: Neuron,
 {
     pub fn predict(&self, input: Vec<f64>) -> Vec<f64> {
-        unimplemented!()
+        self.layers
+            .iter()
+            .fold(input, |acc, layer| layer.forward(&acc))
+    }
+
+    pub fn train(&mut self, input: Vec<f64>, expected: Vec<f64>, learning_rate: f64) {
+        let predicted = self.predict(input);
+        let loss = expected
+            .iter()
+            .zip(predicted.iter())
+            .fold(0.0, |acc, (expected, predicted)| {
+                acc + (expected - predicted).powi(2)
+            });
+
+        let gradient = expected
+            .iter()
+            .zip(predicted.iter())
+            .map(|(expected, predicted)| expected - predicted)
+            .collect::<Vec<f64>>();
+        /*
+        let mut total_loss = 0.0;
+            for (input, expected) in &data {
+                let hidden_layer_outputs = hidden_layer.forward(&input[..]);
+                let predicted = output_layer.forward(&hidden_layer_outputs[..]);
+                let loss = (expected - predicted[0]).powi(2);
+
+                let gradient = expected - predicted[0];
+                let gradient_hidden =
+                    output_layer.backwards(&hidden_layer_outputs[..], &[gradient], learning_rate);
+                let gradient_output =
+                    hidden_layer.backwards(&input[..], &gradient_hidden, learning_rate);
+
+                total_loss += loss;
+            }
+            if epoch % 100 == 0 {
+                println!("Epoch: {}, Loss: {}", epoch, total_loss);
+            }
+         */
     }
 }
