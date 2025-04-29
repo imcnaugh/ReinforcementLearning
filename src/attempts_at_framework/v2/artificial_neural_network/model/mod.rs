0a00febe2c4c57f0a561b9c65bda5637064e43bf
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-04-29 11:07:28 -0500

    starting to test this model, but im fighting rust and i need a water, ill go home and finish up

--- src/attempts_at_framework/v2/artificial_neural_network/model/mod.rs
+++ src/attempts_at_framework/v2/artificial_neural_network/model/mod.rs
@@ -1,7 +1,6 @@
 use crate::attempts_at_framework::v2::artificial_neural_network::layer::Layer;
 use crate::attempts_at_framework::v2::artificial_neural_network::loss_functions::LossFunction;
 use crate::attempts_at_framework::v2::artificial_neural_network::neuron::Neuron;
-use crate::chapter_05::policy::Policy;
 
 pub struct Model<N, L>
 where
@@ -26,7 +25,7 @@ where
     }
 
     pub fn train(&mut self, input: Vec<f64>, expected: Vec<f64>, learning_rate: f64) {
-        let predicted = self.predict(input);
+        let predicted = self.predict(input.clone());
         let new_loss = self.loss_function.calculate_loss(&expected, &predicted);
 
         //TODO verify this is the sam and remove it.
@@ -37,29 +36,40 @@ where
                 acc + (expected - predicted).powi(2)
             });
 
-        let gradient = expected
+        let mut gradient = self
+            .loss_function
+            .calculate_gradient(&expected, &predicted)
             .iter()
             .zip(predicted.iter())
-            .map(|(expected, predicted)| expected - predicted)
+            .zip(self.layers.last().unwrap().get_neurons().iter())
+            .map(|((loss_gradient, predicted), neuron)| {
+                loss_gradient * neuron.activation_derivative(*predicted)
+            })
             .collect::<Vec<f64>>();
-        /*
-        let mut total_loss = 0.0;
-            for (input, expected) in &data {
-                let hidden_layer_outputs = hidden_layer.forward(&input[..]);
-                let predicted = output_layer.forward(&hidden_layer_outputs[..]);
-                let loss = (expected - predicted[0]).powi(2);
+        let mut current_inputs = input;
 
-                let gradient = expected - predicted[0];
-                let gradient_hidden =
-                    output_layer.backwards(&hidden_layer_outputs[..], &[gradient], learning_rate);
-                let gradient_output =
-                    hidden_layer.backwards(&input[..], &gradient_hidden, learning_rate);
+        self.layers.iter_mut().rev().for_each(|layer| {
+            // TODO might need to add to total loss, figure that out
+            current_inputs = layer.forward(&current_inputs);
+            gradient = layer.backwards(&current_inputs, &gradient, learning_rate);
+        })
+    }
+}
 
-                total_loss += loss;
-            }
-            if epoch % 100 == 0 {
-                println!("Epoch: {}, Loss: {}", epoch, total_loss);
-            }
-         */
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::attempts_at_framework::v2::artificial_neural_network::neuron::linear_neuron::LinearNeuron;
+    use crate::attempts_at_framework::v2::artificial_neural_network::neuron::relu_neuron::ReluNeuron;
+
+    #[test]
+    fn test_model() {
+        let output_layer = Layer::<LinearNeuron>::new(1, 2);
+        let hidden_layer = Layer::<ReluNeuron>::new(2, 2);
+
+        let boxed_output_layer = Box::new(output_layer);
+        let boxed_hidden_layer = Box::new(hidden_layer);
+
+        let layers = vec![boxed_hidden_layer, boxed_output_layer];
     }
 }
