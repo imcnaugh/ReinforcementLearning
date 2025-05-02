9257d282ea42da2b34a4889d6f7faee838fa769b
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-05-02 15:10:40 -0500

    so the current v2 state object i have will not work for this as its value function needs to include the action, in place of creting a new state object, ill just raw dawg it

--- src/chapter_10/mountain_car.rs
+++ src/chapter_10/mountain_car.rs
@@ -1,20 +1,39 @@
-struct MountainCar {
-    time_step: usize,
+pub const VELOCITY_LOWER_BOUND: f64 = -0.07;
+pub const VELOCITY_UPPER_BOUND: f64 = 0.07;
+pub const POSITION_LOWER_BOUND: f64 = -1.2;
+pub const POSITION_UPPER_BOUND: f64 = 0.5;
+
+pub struct MountainCar {
     x_position: f64,
     velocity: f64,
 }
 
 impl MountainCar {
+    pub fn new(x_position: f64, velocity: f64) -> Self {
+        Self {
+            x_position: x_position.clamp(POSITION_LOWER_BOUND, POSITION_UPPER_BOUND),
+            velocity: velocity.clamp(VELOCITY_LOWER_BOUND, VELOCITY_UPPER_BOUND),
+        }
+    }
+
     pub fn tick(&mut self, acceleration: f64) {
         let new_velocity = ((self.velocity + (0.001 * acceleration))
             - (0.0025 * f64::cos(3.0 * self.x_position)))
-        .clamp(-0.07, 0.07);
+        .clamp(VELOCITY_LOWER_BOUND, VELOCITY_UPPER_BOUND);
 
-        let new_x_position = (self.x_position + new_velocity).clamp(-1.2, 0.5);
+        let new_x_position =
+            (self.x_position + new_velocity).clamp(POSITION_LOWER_BOUND, POSITION_UPPER_BOUND);
 
         self.velocity = new_velocity;
         self.x_position = new_x_position;
-        self.time_step += 1;
+    }
+
+    pub fn get_x_position(&self) -> f64 {
+        self.x_position
+    }
+
+    pub fn get_velocity(&self) -> f64 {
+        self.velocity
     }
 }
 
@@ -23,23 +42,20 @@ mod tests {
     use super::*;
     use crate::service::{LineChartBuilder, LineChartData};
     use plotters::prelude::full_palette::PURPLE;
-    use plotters::prelude::{ShapeStyle, BLACK, BLUE, GREEN, RED};
+    use plotters::prelude::{ShapeStyle, BLACK, BLUE, RED};
     use std::path::PathBuf;
 
     #[test]
     fn test_mountain_car() {
         let mut go_car = MountainCar {
-            time_step: 0,
             x_position: 0.0,
             velocity: 0.0,
         };
         let mut neutral_car = MountainCar {
-            time_step: 0,
             x_position: 0.0,
             velocity: 0.0,
         };
         let mut reverse_car = MountainCar {
-            time_step: 0,
             x_position: 0.0,
             velocity: 0.0,
         };
@@ -48,7 +64,7 @@ mod tests {
         let mut neutral_car_x_over_time: Vec<(f32, f32)> = Vec::new();
         let mut reverse_car_x_over_time: Vec<(f32, f32)> = Vec::new();
 
-        for tick in 0..1000 {
+        for tick in 0..100 {
             go_car_x_over_time.push((tick as f32, go_car.x_position as f32));
             neutral_car_x_over_time.push((tick as f32, neutral_car.x_position as f32));
             reverse_car_x_over_time.push((tick as f32, reverse_car.x_position as f32));
@@ -74,7 +90,10 @@ mod tests {
         );
         let track_width = LineChartData::new(
             "track width".to_string(),
-            vec![(0.0, -1.2), (0.0, 0.5)],
+            vec![
+                (0.0, POSITION_LOWER_BOUND as f32),
+                (0.0, POSITION_UPPER_BOUND as f32),
+            ],
             ShapeStyle::from(&BLACK),
         );
 
