8859d621dbe3d1fb208941867be8b38e9f44aa95
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-06-28 15:05:13 -0500

    mont carlo value estimation

--- src/chapter_11/example_11_2.rs
+++ src/chapter_11/example_11_2.rs
@@ -1,5 +1,6 @@
 use rand::prelude::IndexedRandom;
 
+#[derive(Clone)]
 struct State {
     id: String,
     next_states: Vec<(f64, State)>,
@@ -54,5 +55,48 @@ mod tests {
             }
             _ => panic!("Unexpected state"),
         }
+    }
+
+    #[test]
+    fn monte_carlo_method_for_value() {
+        let terminal_state_b = State::new("terminal_b".to_string(), vec![], 0.0);
+        let terminal_state_c = State::new("terminal_c".to_string(), vec![], 0.0);
+
+        let state_b = State::new("b".to_string(), vec![(1.0, terminal_state_b)], 1.0);
+        let state_c = State::new("c".to_string(), vec![(0.0, terminal_state_c)], 0.0);
+        let state_a = State::new("a".to_string(), vec![(0.0, state_b), (0.0, state_c)], 0.5);
+
+        let episode_count = 10000;
+        let learning_rate = 0.10;
+        let mut state_a_value = 0.0;
+        let mut state_b_value = 0.0;
+        let mut state_c_value = 0.0;
+
+        for episode_number in (0..episode_count) {
+            let mut history = vec![];
+
+            let mut current_state = state_a.clone();
+
+            while !current_state.is_terminal() {
+                let curr_id = current_state.id.clone();
+                let (reward, next_state) = current_state.transition();
+                let his = (reward.clone(), curr_id);
+                history.push(his);
+                current_state = next_state.clone();
+            }
+
+            let mut total_reward = 0.0;
+            for (reward, state_id) in history.iter().rev() {
+                total_reward += reward;
+                match state_id.as_str() {
+                    "a" => state_a_value += learning_rate * (total_reward - state_a_value),
+                    "b" => state_b_value += learning_rate * (total_reward - state_b_value),
+                    "c" => state_c_value += learning_rate * (total_reward - state_c_value),
+                    _ => panic!("Unexpected state"),
+                }
+            }
+        }
+
+        println!("State A value: {}, State B value: {}, State C value: {}", state_a_value, state_b_value, state_c_value);
     }
 }
