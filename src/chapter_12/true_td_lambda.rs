fa1df72391b2a38518609fbd923eba8fe92030bb
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-07-19 13:18:57 -0500

    yeah that seems to order the states correctly, i dont think the values are ideal, im sure i have a bug somewhere, but the logic seems to be good enough

--- src/chapter_12/true_td_lambda.rs
+++ src/chapter_12/true_td_lambda.rs
@@ -102,3 +102,62 @@ impl<P: Policy, S: State> TrueTdLambda<P, S> {
             .sum()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::attempts_at_framework::v1::policy::RandomPolicy;
+    use crate::service::x_state_walk_environment::{WalkState, WalkStateFactory};
+    use super::*;
+
+    const DEFAULT_VALUE_FUNCTION: fn(WalkState) -> Vec<f64> = |s| vec![s.get_id().parse::<f64>().unwrap()];
+
+    fn generate_polynomial_value_function(
+        total_states: usize,
+        polynomial_degree: usize,
+    ) -> impl Fn(WalkState) -> Vec<f64> {
+        move |state| {
+            let mut response = vec![0.0; polynomial_degree + 1];
+            if !state.is_terminal() {
+                response[0] = 1.0;
+                for i in 0..polynomial_degree {
+                    response[i + 1] = (state.get_id().parse::<f64>().unwrap()
+                        / total_states as f64)
+                        * response[i];
+                }
+            }
+            response
+        }
+    }
+
+    #[test]
+    fn nineteen_step_random_walk_test() {
+        let number_of_episodes = 10000;
+
+        let value_function = generate_polynomial_value_function(19, 1);
+
+        let factory = WalkStateFactory::new(19, 1, &value_function).unwrap();
+
+        let starting_state = factory.get_starting_state();
+        let starting_states = vec![starting_state];
+
+        let policy = RandomPolicy::new();
+
+        let mut true_td_lambda = TrueTdLambda::new(
+            policy,
+            0.001,
+            0.9,
+            0.9,
+            starting_states,
+        );
+
+        (0..number_of_episodes).for_each(|_| true_td_lambda.learn_for_single_episode());
+
+        println!("Weights: {:?}", true_td_lambda.weights);
+        (0..19).for_each(|i| {
+                let (_,state) = factory.generate_state_and_reward_for_id(i);
+                let value = true_td_lambda.get_state_value(&state);
+                println!("State Id {} has value: {}", i, value);
+            }
+        )
+    }
+}
